b1f0c9f2ed10669d82d0e5d035bc97fd45dd08ee
Artem Peregon <ape@ut.in.ua>
2023-06-05 14:50:03 +0300

    add TaskDto and TaskService

---
+++ src/main/java/com/student/work/dto/service/TaskService.java
@@ -0,0 +1,13 @@
+package com.student.work.dto.service;
+
+import com.student.work.entity.Task;
+import com.student.work.exception.TaskNotFoundException;
+
+public interface TaskService {
+    Task createTask(String title, boolean isCompleted);
+    Task updateTask(Long id, String title, boolean isCompleted);
+
+    Task getTaskById(Long id) throws TaskNotFoundException;
+
+    void deleteTaskById(Long id);
+}
